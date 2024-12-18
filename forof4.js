bf8c92c93d3cf4063af9451cd18b6679808c7b44
togrull2000 <togrull2000@gmail.com>
2024-12-18 17:55:25 +0400

    forof4

---
+++ forof4.js
@@ -0,0 +1,9 @@
+const adlar = ['cabir','togrul','ilkin','elmaddin','miri','aysel','hurus']
+for(const ad of adlar) {
+    if(ad.length == 'a' && ad.length == 5) {
+       console.log(`${ad} qatildir`);
+       break
+    }
+    console.log(ad);
+    
+}
